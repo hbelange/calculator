9363d103b1897428833e63507f92c37fc60a86e0
hbelange <harrisonbelanger@gmail.com>
2023-09-01 16:28:33 -0400

    allow user to enter a second number

--- index.js
+++ index.js
@@ -29,10 +29,16 @@ function operate(firstNum, op, secondNum){
 }
 
 const display = document.querySelector('.display');
-let displayValue = 0;
+let displayValue = '';
 
 // populates the display with clicked number or the computed result
 function populateDisplay(e){
+    if (secondNumberStarted){
+        // reset display if the operator has been selected, preceed as normal
+        display.textContent = '';
+        displayValue = '';
+        secondNumberStarted = false;
+    }
     if (display.textContent === '0'){
         display.textContent = '';
     }
@@ -45,7 +51,6 @@ function populateDisplay(e){
 const numbers = document.querySelectorAll('.number')
 numbers.forEach(number => number.addEventListener('click', function(e){
     populateDisplay(e.target.textContent);
-    console.log(displayValue);
 }))
 
 // event listner to clear display
@@ -54,10 +59,26 @@ clear.addEventListener('click', (e) => {
     display.textContent = '0';
 })
 
-// click button
-// make it appear on screen
-// display variable and store it in first number
-// click operation
+// event listener for operator
+const operators = document.querySelectorAll('.operator');
+operators.forEach(op => op.addEventListener('click', (e) => {
+    // whatever numbers are currently stored in the display we want to save
+    firstNumber = display.textContent;
+    // store whichever operation was selected into the operator variable
+    operator = e.target.textContent;
+    secondNumberStarted = true;
+}))
+
+let secondNumberStarted = false;
+
+
+// enter numbers for first number
+// click operation. store the numbers in a variable as well as the operation
+// click numbers for scond number. 
+// click equals, store digits for second number in a variable
+// call the function depening on the operation
+// return the value of the operation and store it in the first variable
+
 
 
 
